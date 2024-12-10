5f636dfceeec4c7a707473b01bf965fb352c4ea6
LeeKyoWon <kskw20@naver.com>
2024-12-10 12:52:44 +0900

    기본 GetMapping, ResponseBody

---
+++ src/main/java/com/ll/basic/domain/home/home/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.ll.basic.domain.home.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    int age = 22;
+
+    // 기본 주소 ( localhost:8080 )
+    @GetMapping("/")
+    @ResponseBody
+    public String showMain() {
+        return "안녕하세요";
+    }
+
+    // 주소 매핑 확인
+    @GetMapping("/about")
+    @ResponseBody
+    public String showAbout() {
+        return "저는 홍길동입니다.";
+    }
+
+}
